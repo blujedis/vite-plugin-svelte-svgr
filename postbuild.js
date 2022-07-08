b874441ad24b1e76727a5eabbb584882284a0389
Blujedis <blujedicorp@gmail.com>
2022-07-08 14:14:37 -0700

    chore(docs): include config example before & after next.345

--- postbuild.js
+++ postbuild.js
@@ -4,10 +4,5 @@
  * define globals. Good enough for this purpose.
  */
 const { appendFileSync, copyFileSync } = require('fs');
-const { join } = require('path');
-const cwd = process.cwd();
-const indexPath = join(cwd, 'dist/types/index.d.ts');
-const modulePath = join(cwd, 'src/ambient.d.ts');
-const typesPath = join(cwd, 'dist/types/ambient.d.ts');
-copyFileSync(modulePath, typesPath);
-appendFileSync(indexPath, `import './ambient';`);
+copyFileSync('./src/ambient.d.ts', './dist/types/ambient.d.ts');
+appendFileSync('./dist/types/index.d.ts', `import './ambient';`);
--- src/ambient.d.ts
+++ src/ambient.d.ts
@@ -1,23 +1,22 @@
-
 declare module '*.svg' {
-  import { SvelteComponent } from 'svelte';
-  const content: SvelteComponent;
-  export default content;
+	import { SvelteComponent } from 'svelte';
+	const content: SvelteComponent;
+	export default content;
 }
 
 declare module '*.svg?component' {
-  import { SvelteComponent } from 'svelte';
-  // removing typeof here will cause JSX to complain.
-  const content: typeof SvelteComponent;
-  export default content;
+	import { SvelteComponent } from 'svelte';
+	// removing typeof here will cause JSX to complain.
+	const content: typeof SvelteComponent;
+	export default content;
 }
 
 declare module '*.svg?raw' {
-  const content: string;
-  export default content;
+	const content: string;
+	export default content;
 }
 
 declare module '*.svg?url' {
-  const content: string;
-  export default content;
+	const content: string;
+	export default content;
 }
